35adf77f8cc242c8a2f0487937841c5a17eea249
KrunalDarekar <96249206+KrunalDarekar@users.noreply.github.com>
2024-05-10 14:43:20 +0530

    Fix image overflow

--- frontend/src/components/ui/CourseCard.tsx
+++ frontend/src/components/ui/CourseCard.tsx
@@ -12,8 +12,8 @@ import LevelBadge from "./LevelBadge";
 const CourseCard = ({course}:{course:Course}) => {
     return (
         <Link to={`/course/${course._id}`}>
-            <Card className="">
-                <CardHeader>
+            <Card className="h-80">
+                <CardHeader className="h-1/3">
                     <div className="flex items-center justify-between">
                         <CardTitle>{course.name}</CardTitle>
                         <LevelBadge level={course.level} />
@@ -22,9 +22,9 @@ const CourseCard = ({course}:{course:Course}) => {
                         course.description.length > 100 ? `${course.description.slice(0, 100)}...` : course.description}
                     </CardDescription>
                 </CardHeader>
-                <CardContent>
-                    <div className="">
-                        <img src={course.image.url} alt="" />
+                <CardContent className="h-2/3">
+                    <div className="h-full w-full">
+                        <img className="w-full h-full object-fit" src={course.image.url} alt="" />
                     </div>
                 </CardContent>
             </Card>
